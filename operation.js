4b896821e49f2d3a2f3632fbfbfdfe37cdbe49e4
Diego Santos Moreira <102436168+DiegoSantosMoreira@users.noreply.github.com>
2022-04-18 15:36:03 -0300

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,29 @@
+function inserir(vlr){
+    var numero = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = numero + vlr;
+}
+function apagarTudo(){
+    document.getElementById('screen').innerHTML = "";
+}
+function voltar(){
+    var numero = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = numero.substring(0,numero.length -1);
+}
+function calcular10(){
+    var numero = parseFloat(document.getElementById('screen').innerHTML);
+    document.getElementById('screenGorjeta').innerHTML = eval(numero * 0.1).toFixed(2);
+    var numero2 = parseFloat(document.getElementById('screenGorjeta').innerHTML);
+    document.getElementById('screenTotal').innerHTML = (numero + numero2).toFixed(2);
+}
+function calcular15(){
+    var numero = parseFloat(document.getElementById('screen').innerHTML);
+    document.getElementById('screenGorjeta').innerHTML = eval(numero * 0.15).toFixed(2);
+    var numero2 = parseFloat(document.getElementById('screenGorjeta').innerHTML);
+    document.getElementById('screenTotal').innerHTML = (numero + numero2).toFixed(2);
+}
+function calcular20(){
+    var numero = parseFloat(document.getElementById('screen').innerHTML);
+    document.getElementById('screenGorjeta').innerHTML = eval(numero * 0.2).toFixed(2);
+    var numero2 = parseFloat(document.getElementById('screenGorjeta').innerHTML);
+    document.getElementById('screenTotal').innerHTML = (numero + numero2).toFixed(2);
+}
