5083b072c82ca81552785761c9f3c02ad0ded289
Diego Santos Moreira <102436168+DiegoSantosMoreira@users.noreply.github.com>
2022-04-22 14:05:39 -0300

    Update operation.js

--- operation.js
+++ operation.js
@@ -1,16 +1,27 @@
+var range = document.getElementById('range');
+range.addEventListener("input", function(){
+    var gorjeta = document.getElementById('screenGorjeta').innerHTML;
+    var vlrTotal = document.getElementById('screenTotal').innerHTML;
+    var x = range.value;
+    document.getElementById('numeroPessoas').innerHTML = x;
+    document.getElementById('p1').innerHTML = eval(gorjeta/x).toFixed(2);
+    document.getElementById('p2').innerHTML = eval(vlrTotal/x).toFixed(2);
+})
+
+
+
 function inserir(vlr){
     var numero = document.getElementById('screen').innerHTML;
     document.getElementById('screen').innerHTML = numero + vlr;
 }
 function apagarTudo(){
     document.getElementById('screen').innerHTML = "";
-    document.getElementById('screenGorjeta').innerHTML = "";
-    document.getElementById('screenTotal').innerHTML = "";
-    
+    document.getElementById('screenGorjeta').innerHTML="";
+    document.getElementById('screenTotal').innerHTML="";
 }
 function voltar(){
     var numero = document.getElementById('screen').innerHTML;
-    document.getElementById('screen').innerHTML = numero.substring(0,numero.length -1);
+    document.getElementById('screen').innerHTML = numero.substring(0,numero.length -1);x
 }
 function calcular10(){
     var numero = parseFloat(document.getElementById('screen').innerHTML);
