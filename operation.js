0ffff203be1b562c37f4a66ebc7f248d79df252e
Diego Santos Moreira <102436168+DiegoSantosMoreira@users.noreply.github.com>
2022-04-25 10:52:09 -0300

    Update operation.js

--- operation.js
+++ operation.js
@@ -1,15 +1,16 @@
-var range = document.getElementById('range');
-range.addEventListener("input", function(){
-    var gorjeta = document.getElementById('screenGorjeta').innerHTML;
-    var vlrTotal = document.getElementById('screenTotal').innerHTML;
-    var x = range.value;
-    document.getElementById('numeroPessoas').innerHTML = x;
-    document.getElementById('p1').innerHTML = eval(gorjeta/x).toFixed(2);
-    document.getElementById('p2').innerHTML = eval(vlrTotal/x).toFixed(2);
-})
+function calcularValorPessoa(){
+var numeroPessoa = document.getElementById('numero1').value;
+    if(numeroPessoa)
+    {
+        var gorjeta = document.getElementById('screenGorjeta').innerHTML;
+        var vlrTotal = document.getElementById('screenTotal').innerHTML;
+        document.getElementById('p1').innerHTML = eval(gorjeta/numeroPessoa).toFixed(2);
+        document.getElementById('p2').innerHTML = eval(vlrTotal/numeroPessoa).toFixed(2);
+    } else(
+        alert("Defina o número de pessoas!")
+    )
 
-
-
+}
 function inserir(vlr){
     var numero = document.getElementById('screen').innerHTML;
     document.getElementById('screen').innerHTML = numero + vlr;
