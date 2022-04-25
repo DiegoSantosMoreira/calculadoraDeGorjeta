1891a71e50a664d0159b3888b4836d1e580c4a08
Diego Santos Moreira <102436168+DiegoSantosMoreira@users.noreply.github.com>
2022-04-25 10:58:05 -0300

    Update operation.js

--- operation.js
+++ operation.js
@@ -19,6 +19,9 @@ function apagarTudo(){
     document.getElementById('screen').innerHTML = "";
     document.getElementById('screenGorjeta').innerHTML="";
     document.getElementById('screenTotal').innerHTML="";
+     document.getElementById('p1').innerHTML="";
+    document.getElementById('p2').innerHTML="";
+    document.getElementById('numero1').value="";
 }
 function voltar(){
     var numero = document.getElementById('screen').innerHTML;
